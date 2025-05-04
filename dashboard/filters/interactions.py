aeb0c32a3e614d26655ad62e58840d4134711bf2
Torch <rustruedetective@gmail.com>
2025-05-04 08:18:12 +0500

    changed filters

--- dashboard/filters/interactions.py
+++ dashboard/filters/interactions.py
@@ -5,7 +5,7 @@ def filter (interactions, customer, cb_args):
 
     interactions = interactions[min_date <= interactions["interaction_date"]]
     if not cb_args["end_date"] is None:
-        interactions = interactions[interactions["interaction   _date"] <= max_date]
+        interactions = interactions[interactions["interaction_date"] <= max_date]
 
     interactions = interactions[interactions["customer_id"].isin(customer_ids)]
 
